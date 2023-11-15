9cb1c25f63e030081e1f46f3cd50a09a464a1bcc
mihret1 <mihlet2@gmail.com>
2023-11-15 23:11:40 +0300

    install latest node version for fix erorr

---
+++ new/app/layout.jsx
@@ -0,0 +1,16 @@
+
+
+const RootLayout=({children})=>{
+    return(
+        <html lang="en">
+            <body>
+                <div></div>
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
---
+++ new/app/page.jsx
@@ -0,0 +1,10 @@
+
+const Home=()=>{
+    return(
+        <div>
+            Home page is this
+        </div>
+    )
+}
+
+export default Home
